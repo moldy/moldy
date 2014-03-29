ad23b5dea6b4518978f8fd6a6623be59a67333a3
David Tsuji <david@flodge.com>
2014-03-29 14:25:23 +1000

    updated the test/headers.js with the new syntax

--- test/headers.js
+++ test/headers.js
@@ -4,11 +4,14 @@ var Model = require( '../src' ),
 describe( 'headers', function () {
 
 	it( 'should be able to add headers', function () {
-		var personModel = new Model( 'person' )
-			.$property( 'name' )
-			.$headers( {
+		var personModel = new Model( 'person', {
+			headers: {
 				'X-API-KEY': 'ec5c6970-a8e3-4294-ef78-48043e88d05b'
-			} );
+			},
+			properties: {
+				name: ''
+			}
+		} );
 
 		personModel.$headers().should.eql( {
 			'X-API-KEY': 'ec5c6970-a8e3-4294-ef78-48043e88d05b'
