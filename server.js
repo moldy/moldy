abf0766db28d224d6771ccd8e1eec9a2f8f39dd9
David Tsuji <david@flodge.com>
2014-08-07 09:17:46 +1000

    updated express

--- server.js
+++ server.js
@@ -2,32 +2,22 @@
  * Module dependencies.
  */
 
-var express = require( 'express' );
-var http = require( 'http' );
-var path = require( 'path' );
-
-var app = express();
-
-// all environments
-app.set( 'port', process.env.PORT || 3000 );
-app.set( 'views', path.join( __dirname, 'views' ) );
-app.set( 'view engine', 'jade' );
-app.use( express.favicon() );
-app.use( express.logger( 'dev' ) );
-app.use( express.json() );
-app.use( express.urlencoded() );
-app.use( express.methodOverride() );
-app.use( app.router );
-app.use( express.static( path.join( __dirname, 'public' ) ) );
-
-// development only
-if ( 'development' == app.get( 'env' ) ) {
-	app.use( express.errorHandler() );
-}
+var express = require( 'express' ),
+	bodyParser = require( 'body-parser' ),
+	logger = require( 'morgan' ),
+	debug = require( 'debug' )( process.env.APP_NAME );
 
 var _ = require( 'underscore' ),
 	guid = require( 'sc-guid' ),
 	dummy = require( './test/dummy' );
+
+var app = express();
+
+// app.use( logger( 'dev' ) );
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( {
+	extended: false
+} ) );
 
 app.get( '/api/person/:id', function ( _req, _res ) {
 	var person = _.findWhere( dummy.data.people, {
@@ -49,26 +39,30 @@ app.put( '/api/person/:id', function ( _req, _res ) {
 	_res.json( _req.body );
 } );
 
-app.del( '/api/person/:id', function ( _req, _res ) {
+app.delete( '/api/person/:id', function ( _req, _res ) {
 	_res.json();
 } );
 
 app.post( '/empty-body/*', function ( _req, _res ) {
-	_res.send( 200 );
+	_res.send( 200 ).end();
 } );
 
 app.post( '/400/*', function ( _req, _res ) {
-	_res.send( 400 );
+	_res.send( 400 ).end();
 } );
 
 app.post( '/404/*', function ( _req, _res ) {
-	_res.send( 404 );
+	_res.send( 404 ).end();
 } );
 
 app.post( '/500/*', function ( _req, _res ) {
-	_res.send( 500 );
+	_res.send( 500 ).end();
 } );
 
-http.createServer( app ).listen( app.get( 'port' ), function () {
-	console.log( 'Express server listening on port ' + app.get( 'port' ) );
+app.set( 'port', process.env.PORT || 3000 );
+
+var server = app.listen( app.get( 'port' ), function () {
+	debug( 'Express server listening on port ' + server.address().port );
 } );
+
+module.exports = app;
